b959eb4768b0f61eb7c056fb4d9d899fc4029b6f
duc-la <duclagithub@gmail.com>
2024-05-19 15:46:59 -0700

    Start Level Up Planning

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Header } from "./Header"
+
+import "./App.css"
+
+export default function App() {
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
---
+++ src/Header.tsx
@@ -0,0 +1,19 @@
+import "./Header.css"
+
+export function Header() {
+  return (
+    <>
+      <div className="header">
+        <div className="company">
+          <p className="name">Level Up Planning</p>
+          {/*<input className="logo" type="image" src="./src/images/process.png"/>*/}
+        </div>
+
+        {/*Take image from google account later*/}
+        <div className="account-profile">
+          
+        </div>
+      </div>
+    </>
+  )
+}
